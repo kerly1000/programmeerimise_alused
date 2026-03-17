9640d23d7ac35f79129a62574b94f5022298e39f
Opilane TTHK <opilane@tthk.ee>
2026-03-17 11:46:06 +0200

    testimine

--- 04_matemaatilised_avaldised/test_quadratic_equation.py
+++ 04_matemaatilised_avaldised/test_quadratic_equation.py
@@ -1,5 +1,13 @@
-import pytest
 from quadratic_equation import solve_quadratic_equation as solve
 
 def test_quadratic_equation():
     assert solve(1, -3, 2) == (1, 2)
+
+def test_float_values():
+    assert solve(1, -4, 3.75) == (1.5, 2.5)
+
+def test_one_solution():
+    assert solve(1, -4, 4) == (2,)
+
+def test_zero_solution():
+    assert solve(10, 2, 1) == tuple()
