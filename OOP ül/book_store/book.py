2fff3cecc7a1f6535d5475db09c3c2fda7764eaf
kerly1000 <kerly1000@hotmail.com>
2026-04-20 06:32:31 +0300

    book store

---
+++ OOP ül/book_store/book.py
@@ -0,0 +1,24 @@
+"""Book store."""
+
+
+class Book:
+    """Represent book model."""
+
+    def __init__(self, title: str, author: str, price: float, rating: float):
+        """
+        Class constructor. Each book has title, author and price.
+
+        :param title: book's title
+        :param author: book's author
+        :param price: book's price
+        """
+        self.title = title
+        self.author = author
+        self.price = price
+        self.rating = rating
+
+    def __eq__(self, value):
+        """Check if is same object."""
+        if isinstance(value, Book):
+            return self.author == value.author and self.title == value.title
+        return False
